fb5a1c6b8c3028e4830eb8925964321d559e61da
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-06-22 15:41:47 -0300

    first commit

---
+++ backend/src/__test__/example.spec.ts
@@ -0,0 +1,6 @@
+describe("Example test", () => {
+    it("should add two numbers correctly", () => {
+        const sum = (a: number, b: number) => a + b;
+        expect(sum(1, 2)).toBe(3);
+    });
+});
