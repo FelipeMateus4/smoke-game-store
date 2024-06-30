ed86243c9f39d9ea0b104d88f1c4fc102d826a10
Gabriel Silva <107601526+Kyzuke@users.noreply.github.com>
2024-06-30 03:56:14 -0300

    Novo header

--- frontend/src/components/HeaderMain/Header.js
+++ frontend/src/components/HeaderMain/Header.js
@@ -3,54 +3,55 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faGamepad, faInfoCircle, faHeadset, faUser } from "@fortawesome/free-solid-svg-icons";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import "./Header.css"; // Importando o arquivo de estilos CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css"; // Importa o arquivo CSS
+import logo from "../../assets/da.jpg"; // Importa o logo da loja
 
-function Header() {
+const Header = () => {
     return (
-        <Navbar expand="xl" variant="dark" className="navbar-custom">
-            <Navbar.Brand as={Link} to="/" className="mr-auto">
-                <FontAwesomeIcon icon={faHome} /> <span className="brand-text">Smoke Games Store</span>
+        <Navbar expand="xl" className="navbar-custom">
+            <Navbar.Brand as={Link} to="/">
+                <img src={logo} height="40" className="d-inline-block align-top" alt="Loja Logo" /> SmokeGames Store
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mx-auto">
-                    <Nav.Link as={Link} to="/">
-                        <FontAwesomeIcon icon={faHome} /> Destaques
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
+                <Nav className="mx-auto mx-custom">
+                    <Nav.Link as={Link} to="/destaques">
+                        <FontAwesomeIcon icon={faHome} className="fa-icon" /> Destaques
                     </Nav.Link>
                     <NavDropdown
                         title={
-                            <>
-                                <FontAwesomeIcon icon={faGamepad} /> Produtos
-                            </>
+                            <span>
+                                <FontAwesomeIcon icon={faGamepad} className="fa-icon" /> Produtos
+                            </span>
                         }
                         id="basic-nav-dropdown"
                     >
-                        <NavDropdown.Item as={Link} to="/category1">
+                        <NavDropdown.Item as={Link} to="/produtos/categoria1">
                             Categoria 1
                         </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/category2">
+                        <NavDropdown.Item as={Link} to="/produtos/categoria2">
                             Categoria 2
                         </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item as={Link} to="/category3">
+                        <NavDropdown.Item as={Link} to="/produtos/categoria3">
                             Categoria 3
                         </NavDropdown.Item>
                     </NavDropdown>
-                    <Nav.Link as={Link} to="/about">
-                        <FontAwesomeIcon icon={faInfoCircle} /> Sobre
+                    <Nav.Link as={Link} to="/sobre">
+                        <FontAwesomeIcon icon={faInfoCircle} className="fa-icon" /> Sobre
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/contact">
-                        <FontAwesomeIcon icon={faHeadset} /> Suporte
+                    <Nav.Link as={Link} to="/contato">
+                        <FontAwesomeIcon icon={faHeadset} className="fa-icon" /> Contato
                     </Nav.Link>
                 </Nav>
-                <Nav className="ml-auto mt-2">
-                    <Nav.Link as={Link} to="/account/login">
-                        <FontAwesomeIcon icon={faUser} /> Entrar
+                <Nav>
+                    <Nav.Link as={Link} to="/entrar">
+                        <FontAwesomeIcon icon={faUser} className="fa-icon" /> Entrar
                     </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
     );
-}
+};
 
 export default Header;
