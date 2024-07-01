ba6bdd884fff094568e45e30ebac5238a95f89c4
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-07-01 17:54:04 -0300

    backgournd home

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -3,10 +3,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/HeaderMain/Header";
 import Footer from "../../components/Footer/Footer";
+import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-background">
             <Header />
             <Footer />
             <p></p>
